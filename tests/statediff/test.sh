ea575cc6b60839bdf735697be873ce2e402abfd3
Kevin Assogba <43794220+kevinassogba@users.noreply.github.com>
2025-04-03 12:39:23 -0400

    Update test.sh

--- tests/statediff/test.sh
+++ tests/statediff/test.sh
@@ -3,14 +3,14 @@
 set -e
 
 echo "Activating Spack environment..."
-source $(spack location -i statediff)/share/spack/setup-env.sh
+source $(spack location -i statediff-env)/share/spack/setup-env.sh
 
 echo "Compiling test program..."
-g++ -o test_statediff test.cpp -I$(spack location -i statediff)/include \
-    -L$(spack location -i statediff)/lib -lstatediff
+g++ -o test_statediff test.cpp -I$(spack location -i statediff-env)/include \
+    -L$(spack location -i statediff-env)/lib -lstatediff
 
 echo "Adding statediff to the library path..."
-export LD_LIBRARY_PATH=$(spack location -i statediff)/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$(spack location -i statediff-env)/lib:$LD_LIBRARY_PATH
 
 echo "Running tests..."
 ./test_statediff
